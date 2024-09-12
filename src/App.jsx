91f7aea295e5f3762989acd863e60827a7d103ce
Gedaias <gedaias@geda.dev>
2024-09-11 22:50:35 -0600

    Create footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./styles/App.css";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2020 React Store. All rights reserved</p>
+      <div className="footer-links">
+        <p>Terms of Service</p>
+        <p>Privacy</p>
+      </div>
+    </footer>
+  );
+}
