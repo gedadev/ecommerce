8ec0e24e39f5c144e65c8bfe0fa43afc0eced20b
Gedaias <gedaias@geda.dev>
2024-09-11 22:43:38 -0600

    Create featured products section and the product card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,7 @@
 import "../styles/Home.css";
+import { FaStar } from "react-icons/fa";
+import { IconContext } from "react-icons";
+import { FaShoppingCart } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -10,6 +13,33 @@ export default function Home() {
           experience.
         </p>
         <button>Shop Now</button>
+      </section>
+      <section className="featured">
+        <h2>Featured Products</h2>
+        <div className="featured-products-container">
+          <article className="product-card">
+            <div className="image-card">
+              <img src="" alt="" />
+              <span>Low stock: Only 5 left</span>
+            </div>
+            <div className="product-card-content">
+              <h3>Product Name</h3>
+              <div className="product-value">
+                <p className="product-price">$99.99</p>
+                <p>
+                  <IconContext.Provider value={{ color: "rgb(247, 184, 1)" }}>
+                    <FaStar className="icon" />
+                  </IconContext.Provider>
+                  4.5
+                </p>
+              </div>
+              <button>
+                <FaShoppingCart className="icon" />
+                Add to Cart
+              </button>
+            </div>
+          </article>
+        </div>
       </section>
     </main>
   );
