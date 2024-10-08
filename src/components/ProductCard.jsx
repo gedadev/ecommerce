cf5e62e5456d4d7266a7933970e74d873cbd53c9
Gedaias <gedaias@geda.dev>
2024-10-07 21:17:49 -0600

    Create product card component

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,32 @@
+import { FaStar } from "react-icons/fa";
+import { IconContext } from "react-icons";
+import { FaShoppingCart } from "react-icons/fa";
+
+export default function ProductCard({ product }) {
+  return (
+    <article className="product-card">
+      <div className="image-card">
+        <img src={product.imgSrc} alt="" />
+        <span>
+          {product.availabilityStatus}: Only {product.stock} left
+        </span>
+      </div>
+      <div className="product-card-content">
+        <h3>{product.title}</h3>
+        <div className="product-value">
+          <p className="product-price">${product.price}</p>
+          <p>
+            <IconContext.Provider value={{ color: "rgb(247, 184, 1)" }}>
+              <FaStar className="icon" />
+            </IconContext.Provider>
+            {product.rating}
+          </p>
+        </div>
+        <button>
+          <FaShoppingCart className="icon" />
+          Add to Cart
+        </button>
+      </div>
+    </article>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import "../styles/Home.css";
-import { FaStar } from "react-icons/fa";
-import { IconContext } from "react-icons";
-import { FaShoppingCart } from "react-icons/fa";
+import { useState } from "react";
+import ProductCard from "../components/ProductCard";
+import data from "../mocks/products.json";
 
 export default function Home() {
+  const [products, setProducts] = useState(data.products);
+
   return (
     <main>
+      {console.log(products)}
       <section className="hero">
         <h1>Welcome to React Store</h1>
         <p>
@@ -17,28 +20,9 @@ export default function Home() {
       <section className="featured">
         <h2>Featured Products</h2>
         <div className="featured-products-container">
-          <article className="product-card">
-            <div className="image-card">
-              <img src="" alt="" />
-              <span>Low stock: Only 5 left</span>
-            </div>
-            <div className="product-card-content">
-              <h3>Product Name</h3>
-              <div className="product-value">
-                <p className="product-price">$99.99</p>
-                <p>
-                  <IconContext.Provider value={{ color: "rgb(247, 184, 1)" }}>
-                    <FaStar className="icon" />
-                  </IconContext.Provider>
-                  4.5
-                </p>
-              </div>
-              <button>
-                <FaShoppingCart className="icon" />
-                Add to Cart
-              </button>
-            </div>
-          </article>
+          {products.map((product) => (
+            <ProductCard product={product} key={product.id} />
+          ))}
         </div>
       </section>
     </main>
