0a190183ca18f7beee394d08543437a3760bd8b9
Gedaias <gedaias@geda.dev>
2024-12-26 18:51:21 -0600

    feat: create custom hook for product fetching

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,27 +1,11 @@
 import "../styles/Products.css";
-import { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
 import { Filter } from "../components/Filter";
 import FiltersProvider from "../context/FiltersContext";
+import useProducts from "../hooks/useProducts";
 
 export default function Products() {
-  const [products, setProducts] = useState(null);
-  const [filtersValues, setFiltersValues] = useState(null);
-
-  useEffect(() => {
-    fetch("https://dummyjson.com/products?limit=50")
-      .then((res) => res.json())
-      .then((data) => {
-        const products = data.products;
-        const categories = [
-          ...new Set(products.map((product) => product.category)),
-        ];
-        const brand = [...new Set(products.map((product) => product.brand))];
-
-        setFiltersValues([{ categories }, { brand }]);
-        setProducts(products);
-      });
-  }, []);
+  const { products, filtersValues } = useProducts();
 
   return (
     <main className="products-section">
