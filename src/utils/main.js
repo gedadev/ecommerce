c38d82d0e7f7444f80958ed318e15e2e2417c9d5
Gedaias <gedaias@geda.dev>
2024-12-27 15:40:32 -0600

    feat: update formatValue and formatText functions to handle null values

--- src/utils/main.js
+++ src/utils/main.js
@@ -1,10 +1,13 @@
 export const formatWord = (word) =>
   word ? word.charAt(0).toUpperCase() + word.slice(1) : null;
 
-export const formatValue = (value) => value.split(" ").join("-").toLowerCase();
+export const formatValue = (value) =>
+  value ? value.split(" ").join("-").toLowerCase() : "other";
 
 export const formatText = (value) =>
   value
-    .split(/[ -]+/)
-    .map((word) => formatWord(word))
-    .join(" ");
+    ? value
+        .split(/[ -]+/)
+        .map((word) => formatWord(word))
+        .join(" ")
+    : "other";
