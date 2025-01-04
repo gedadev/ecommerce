e9964e51f16f866c6c892526e7f33f8dc63074bc
Gedaias <gedaias@geda.dev>
2025-01-04 13:40:18 -0600

    feat: refactor Home component to separate Hero and FeaturedProducts sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,23 @@
 import "../styles/Home.css";
-import { useState } from "react";
-import ProductCard from "../components/ProductCard";
-import data from "../mocks/products.json";
+import { FeaturedProducts } from "../components/FeaturedProducts";
 
 export default function Home() {
-  const [products, setProducts] = useState(data.products);
-
   return (
     <main>
-      {console.log(products)}
-      <section className="hero">
-        <h1>Welcome to React Store</h1>
-        <p>
-          Discover our amazing products and enjoy a seamless shopping
-          experience.
-        </p>
-        <button>Shop Now</button>
-      </section>
-      <section className="featured">
-        <h2>Featured Products</h2>
-        <div className="featured-products-container">
-          {products.map((product) => (
-            <ProductCard product={product} key={product.id} />
-          ))}
-        </div>
-      </section>
+      <Hero />
+      <FeaturedProducts />
     </main>
+  );
+}
+
+function Hero() {
+  return (
+    <section className="hero">
+      <h1>Welcome to React Store</h1>
+      <p>
+        Discover our amazing products and enjoy a seamless shopping experience.
+      </p>
+      <button>Shop Now</button>
+    </section>
   );
 }
