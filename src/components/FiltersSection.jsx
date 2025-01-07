b59d489b597eeae612c3aebb3b7340353b9a71e7
Gedaias <gedaias@geda.dev>
2025-01-07 14:10:25 -0600

    feat: enhance FiltersSection for mobile responsiveness

--- src/components/FiltersSection.jsx
+++ src/components/FiltersSection.jsx
@@ -1,17 +1,53 @@
 import useFilters from "../hooks/useFilters";
 import { Filter } from "./Filter";
+import { useWindowInfo } from "../hooks/useWindowInfo";
+import { MdFilterList } from "react-icons/md";
+import { FaWindowClose } from "react-icons/fa";
+import { useState } from "react";
 
 export function FiltersSection() {
   const { filtersValues } = useFilters();
+  const { width } = useWindowInfo();
 
   return (
     <section className="product-filters">
       <OrderBy />
-      {filtersValues &&
+      {width > 900 ? (
+        filtersValues &&
         filtersValues.map((values, index) => (
           <Filter key={index} values={values} />
-        ))}
+        ))
+      ) : (
+        <MobileFilter />
+      )}
     </section>
+  );
+}
+
+function MobileFilter() {
+  const { filtersValues } = useFilters();
+  const [filterActive, setFilterActive] = useState(false);
+
+  const handleMobileFilter = () => {
+    setFilterActive(!filterActive);
+  };
+
+  return (
+    <div className="mobile-filter">
+      <MdFilterList className="filter-icon" onClick={handleMobileFilter} />
+      <div
+        className="filters-container"
+        style={{
+          transform: filterActive ? "translateX(0)" : "translateX(100%)",
+        }}
+      >
+        <FaWindowClose className="close-icon" onClick={handleMobileFilter} />
+        {filtersValues &&
+          filtersValues.map((values, index) => (
+            <Filter key={index} values={values} />
+          ))}
+      </div>
+    </div>
   );
 }
 
