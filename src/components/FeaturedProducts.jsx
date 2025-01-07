9a83c5b1f8cf0ec9bf1d86c42b36d4caf0f9d2a1
Gedaias <gedaias@geda.dev>
2025-01-06 18:22:03 -0600

    feat: update FeatureProducts component adding buttons to scroll the section horizontally

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -1,8 +1,36 @@
+import { useState } from "react";
 import useProducts from "../hooks/useProducts";
 import { ProductCard } from "./ProductCard";
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
 
 export function FeaturedProducts() {
   const { featuredProducts } = useProducts({ quantity: 6 });
+  const [leftArrowActive, setLeftArrowActive] = useState(false);
+  const [rightArrowActive, setRightArrowActive] = useState(true);
+
+  const handleLeftScroll = () => {
+    const container = document.querySelector(".featured-products-container");
+    const { children } = container;
+
+    Object.values(children).forEach((child) => {
+      child.style.transform = `translate(${0}px)`;
+      setLeftArrowActive(false);
+      setRightArrowActive(true);
+    });
+  };
+
+  const handleRightScroll = () => {
+    const container = document.querySelector(".featured-products-container");
+    const { children, scrollWidth, clientWidth } = container;
+    const scrollAmount = scrollWidth - clientWidth;
+
+    Object.values(children).forEach((child) => {
+      child.style.transform = `translate(-${scrollAmount}px)`;
+      setLeftArrowActive(true);
+      setRightArrowActive(false);
+    });
+  };
 
   return (
     <section className="featured-section">
@@ -14,6 +42,14 @@ export function FeaturedProducts() {
               <ProductCard product={product} key={product.id} />
             ))}
         </div>
+        <IoIosArrowBack
+          className={`arrow-back ${!leftArrowActive ? "disabled" : ""}`}
+          onClick={handleLeftScroll}
+        />
+        <IoIosArrowForward
+          className={`arrow-fw ${!rightArrowActive ? "disabled" : ""}`}
+          onClick={handleRightScroll}
+        />
       </div>
     </section>
   );
