4df7b36f13250ad4ba074be4d5e2774369347109
Gedaias <gedaias@geda.dev>
2025-01-11 22:14:32 -0600

    feat: add increment quantity function to CartContext

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -13,12 +13,33 @@ export default function CartProvider({ children }) {
         return prevCart;
       }
 
-      return [...prevCart, product];
+      return [...prevCart, { ...product, quantity: 1 }];
     });
   };
 
+  const incrementQuantity = (id) => {
+    setCart((prevCart) => {
+      const newCart = prevCart.map((item) => {
+        const newItem = { ...item };
+        if (newItem.id === id) {
+          newItem.quantity = newItem.quantity + 1;
+        }
+
+        return newItem;
+      });
+
+      return newCart;
+    });
+  };
+
+  const decreaseQuantity = () => {
+    console.log("less");
+  };
+
   return (
-    <CartContext.Provider value={{ cart, addToCart }}>
+    <CartContext.Provider
+      value={{ cart, addToCart, incrementQuantity, decreaseQuantity }}
+    >
       {children}
     </CartContext.Provider>
   );
