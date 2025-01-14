034e42460c69cd146ad0d30a07367f1e97891021
Gedaias <gedaias@geda.dev>
2025-01-14 15:00:55 -0600

    feat: implement product detail fetching and display structure

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,3 +1,34 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import useProducts from "../hooks/useProducts";
+import "../styles/Detail.css";
+
 export default function ProductDetail() {
-  return <>Detail</>;
+  const [product, setProduct] = useState(null);
+  const { id } = useParams();
+  const { products } = useProducts({ limit: 100 });
+
+  useEffect(() => {
+    if (!products) {
+      return;
+    }
+
+    setProduct(() => {
+      const foundProduct = products.find(
+        (product) => Number(product.id) === Number(id)
+      );
+
+      if (!foundProduct) return null;
+
+      return foundProduct;
+    });
+  }, [id, products]);
+
+  return (
+    <main className="product-detail">
+      <section className="product-info"></section>
+      <section className="product-description"></section>
+      <section className="product-reviews"></section>
+    </main>
+  );
 }
