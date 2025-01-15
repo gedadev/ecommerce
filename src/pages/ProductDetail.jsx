3210f9a0a8b5ec2bad9f70e77ca725a1819f4284
Gedaias <gedaias@geda.dev>
2025-01-14 20:27:41 -0600

    feat: refactor ProductDetail component to use DetailProvider for context management

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,35 +1,15 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import useProducts from "../hooks/useProducts";
-import { ProductDetailInfo } from "../components/ProductDetailInfo";
 import "../styles/Detail.css";
+import DetailProvider from "../context/DetailContext";
+import { ProductDetailInfo } from "../components/ProductDetailInfo";
 
 export default function ProductDetail() {
-  const [product, setProduct] = useState(null);
-  const { id } = useParams();
-  const { products } = useProducts({ limit: 100 });
-
-  useEffect(() => {
-    if (!products) {
-      return;
-    }
-
-    setProduct(() => {
-      const foundProduct = products.find(
-        (product) => Number(product.id) === Number(id)
-      );
-
-      if (!foundProduct) return null;
-
-      return foundProduct;
-    });
-  }, [id, products]);
-
   return (
-    <main className="product-detail">
-      <ProductDetailInfo product={product} />
-      <section className="product-description"></section>
-      <section className="product-reviews"></section>
-    </main>
+    <DetailProvider>
+      <main className="product-detail">
+        <ProductDetailInfo />
+        <section className="product-description"></section>
+        <section className="product-reviews"></section>
+      </main>
+    </DetailProvider>
   );
 }
