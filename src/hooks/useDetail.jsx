10cbb67068b5d40f47d76293761a38da0ca8f953
Gedaias <gedaias@geda.dev>
2025-01-14 20:48:26 -0600

    refactor: remove DetailContext and useDetail hook as they are no longer needed

--- src/hooks/useDetail.jsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { DetailContext } from "../context/DetailContext";
-
-export default function useDetail() {
-  const context = useContext(DetailContext);
-
-  return context;
-}
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,15 +1,12 @@
 import "../styles/Detail.css";
-import DetailProvider from "../context/DetailContext";
 import { ProductDetailInfo } from "../components/ProductDetailInfo";
 
 export default function ProductDetail() {
   return (
-    <DetailProvider>
-      <main className="product-detail">
-        <ProductDetailInfo />
-        <section className="product-description"></section>
-        <section className="product-reviews"></section>
-      </main>
-    </DetailProvider>
+    <main className="product-detail">
+      <ProductDetailInfo />
+      <section className="product-description"></section>
+      <section className="product-reviews"></section>
+    </main>
   );
 }
