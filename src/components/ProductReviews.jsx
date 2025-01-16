7908397161bbada4f84eb76fd0e37329dca50d00
Gedaias <gedaias@geda.dev>
2025-01-15 21:25:35 -0600

    feat: enhance ProductReviews component to display overall rating and improve layout

--- src/components/ProductReviews.jsx
+++ src/components/ProductReviews.jsx
@@ -9,18 +9,29 @@ export function ProductReviews() {
 
   return (
     <section className="product-reviews">
-      {product &&
-        product.reviews.map((review, i) => (
-          <div key={i}>
-            <h3>{review.reviewerName}</h3>
-            <span>
-              Reviewed on {formatDate(review.date).date},{" "}
-              {formatDate(review.date).time}
-            </span>
-            <RatingStars length={review.rating} />
-            <p>{review.comment}</p>
+      <div className="rating">
+        <h2>Customer reviews</h2>
+        {product && (
+          <div>
+            <RatingStars length={Math.round(product.rating)} />
+            <p>{product.rating}</p>
           </div>
-        ))}
+        )}
+      </div>
+      <div className="reviews">
+        {product &&
+          product.reviews.map((review, i) => (
+            <div key={i}>
+              <h3>{review.reviewerName}</h3>
+              <span>
+                Reviewed on {formatDate(review.date).date},{" "}
+                {formatDate(review.date).time}
+              </span>
+              <RatingStars length={review.rating} />
+              <p>{review.comment}</p>
+            </div>
+          ))}
+      </div>
     </section>
   );
 }
