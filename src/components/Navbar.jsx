6fe5b16761e6caf120cad5e34f26e6b61895d379
Gedaias <gedaias@geda.dev>
2025-01-21 21:07:39 -0600

    feat: add login modal to Navbar component for user authentication

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,15 @@ import { FaOpencart } from "react-icons/fa";
 import { TiShoppingCart } from "react-icons/ti";
 import { Link } from "react-router-dom";
 import useCart from "../hooks/useCart";
+import { useState } from "react";
 
 export default function Navbar() {
   const { cart } = useCart();
+  const [activeModal, setActiveModal] = useState(false);
+
+  const handleModal = () => {
+    setActiveModal(!activeModal);
+  };
 
   return (
     <nav className="navbar">
@@ -17,9 +23,10 @@ export default function Navbar() {
         <Link to={`products/`}>
           <li>Shop Now</li>
         </Link>
-        <Link to={`login/`}>
-          <li>Sign In</li>
-        </Link>
+        <div className="login-link">
+          <li onClick={handleModal}>Login</li>
+          {activeModal && <LoginModal />}
+        </div>
         <Link to={`cart/`}>
           <li className="cart-icon">
             <TiShoppingCart style={{ fontSize: "1.3rem" }} />
@@ -30,5 +37,27 @@ export default function Navbar() {
         </Link>
       </ul>
     </nav>
+  );
+}
+
+function LoginModal() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="login-modal">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
+        </div>
+        <button>Login</button>
+      </form>
+    </div>
   );
 }
