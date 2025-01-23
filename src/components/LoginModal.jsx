fa17842d64512882ca23b0b19518820a3a59f2d1
Gedaias <gedaias@geda.dev>
2025-01-23 14:10:17 -0600

    feat: enhance LoginModal to display customer profile when logged in and add styling for customer modal

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -4,7 +4,7 @@ import useCustomer from "../hooks/useCustomer";
 export function LoginModal() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const { authUser } = useCustomer();
+  const { authUser, isLoggedIn } = useCustomer();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -14,33 +14,50 @@ export function LoginModal() {
 
   return (
     <div className="login-modal">
-      <form onSubmit={handleSubmit}>
-        <h3>Login</h3>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={username}
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button>Login</button>
-        <p>
-          New customer? <span>Click here!</span>
-        </p>
-      </form>
+      {isLoggedIn ? (
+        <CustomerModal />
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <h3>Login</h3>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button>Login</button>
+          <p>
+            New customer? <span>Click here!</span>
+          </p>
+        </form>
+      )}
+    </div>
+  );
+}
+
+function CustomerModal() {
+  const { customer, logout } = useCustomer();
+
+  return (
+    <div className="customer-modal">
+      {customer && <p>Welcome, {customer.username}</p>}
+      <span></span>
+      <h3>My Profile</h3>
+      <button onClick={logout}>Logout</button>
     </div>
   );
 }
