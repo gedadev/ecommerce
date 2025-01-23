ad08ce37bb22f66ca7f88f004cb3a2ca8cd71fa6
Gedaias <gedaias@geda.dev>
2025-01-23 17:53:40 -0600

    feat: implement Profile page with header and menu components

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,3 +1,41 @@
+import useCustomer from "../hooks/useCustomer";
+import "../styles/Profile.css";
+
 export default function Profile() {
-  return <>profile</>;
+  return (
+    <main className="profile-section">
+      <ProfileHeader />
+      <ProfileMenu />
+    </main>
+  );
+}
+
+function ProfileHeader() {
+  const { customer } = useCustomer();
+
+  return (
+    <header>
+      {customer && (
+        <>
+          <h3>Welcome, {customer.username}</h3>
+          <p>{customer.email}</p>
+        </>
+      )}
+    </header>
+  );
+}
+
+function ProfileMenu() {
+  return (
+    <aside>
+      <ul className="profile-menu">
+        <li>Summary</li>
+        <li>Personal Information</li>
+        <li>Shop History</li>
+        <li>Payment Methods</li>
+        <li>Addresses</li>
+        <li>Logout</li>
+      </ul>
+    </aside>
+  );
 }
