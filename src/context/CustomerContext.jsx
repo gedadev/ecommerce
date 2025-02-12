ac07802b1121dae27847ba4431db458061be841a
Gedaias <gedaias@geda.dev>
2025-02-11 19:11:27 -0600

    feat: fetch and store user data in CustomerContext for orders, addresses, and payment methods

--- src/context/CustomerContext.jsx
+++ src/context/CustomerContext.jsx
@@ -1,5 +1,5 @@
-import { createContext, useState } from "react";
-import { authService } from "../utils/main";
+import { createContext, useEffect, useState } from "react";
+import { authService, getUserData } from "../utils/main";
 
 export const CustomerContext = createContext();
 
@@ -8,6 +8,22 @@ export default function CustomerProvider({ children }) {
   const [token, setToken] = useState(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [errorMsg, setErrorMsg] = useState("");
+  const [orders, setOrders] = useState([]);
+  const [addresses, setAddresses] = useState([]);
+  const [paymentMethods, setPaymentsMethods] = useState([]);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      if (isLoggedIn) {
+        const userData = await getUserData(token);
+        setOrders(userData.orders);
+        setAddresses(userData.addresses);
+        setPaymentsMethods(userData.paymentMethods);
+      }
+    };
+
+    fetchUserData();
+  }, [isLoggedIn, token]);
 
   const authUser = async (credentials) => {
     setErrorMsg("");
@@ -39,6 +55,9 @@ export default function CustomerProvider({ children }) {
         token,
         errorMsg,
         logout,
+        orders,
+        addresses,
+        paymentMethods,
       }}
     >
       {children}
