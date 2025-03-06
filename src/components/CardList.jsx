8997127ce2d4c5ec3a6e86df0e834b6d921bc139
Gedaias <gedaias@geda.dev>
2025-03-05 22:32:07 -0600

    feat: enhance NewCardForm to improve toggle functionality and label visibility

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import useCustomer from "../hooks/useCustomer";
 import { formatValue } from "../utils/main";
 import { IoSettings } from "react-icons/io5";
@@ -48,11 +48,19 @@ export function NewCard({ toggleForm }) {
 }
 
 export function NewCardForm({ toggleForm }) {
+  const toggleRef = useRef(null);
+
+  useEffect(() => {
+    toggleRef.current?.click();
+    console.log(toggleRef.current);
+  }, []);
+
   const toggleType = (e) => {
     const { target } = e;
     if (!target.matches("input")) return;
 
-    const slider = target.parentNode.lastChild;
+    const container = target.parentNode;
+    const slider = container.lastChild;
 
     if (target.id === "debit") {
       slider.style.transform = "translateX(100%)";
@@ -60,12 +68,25 @@ export function NewCardForm({ toggleForm }) {
     if (target.id === "credit") {
       slider.style.transform = "translateX(0)";
     }
+
+    const currentLabel = [...container.children].find(
+      (element) => element.htmlFor && element.htmlFor === target.id
+    );
+    const oppositeLabel = [...container.children].find(
+      (element) => element.htmlFor && element.htmlFor !== target.id
+    );
+    if (currentLabel && oppositeLabel) {
+      currentLabel.style.opacity = "1";
+      oppositeLabel.style.opacity = "0.5";
+    }
   };
 
   return (
     <form className="new-card-form">
       <div onClick={toggleType} className="card-type-toggle">
-        <label htmlFor="credit">Credit</label>
+        <label htmlFor="credit" ref={toggleRef}>
+          Credit
+        </label>
         <input type="radio" name="type" id="credit" defaultChecked />
         <label htmlFor="debit">Debit</label>
         <input type="radio" name="type" id="debit" />
@@ -84,11 +105,11 @@ export function NewCardForm({ toggleForm }) {
         <input type="text" id="expiration" />
       </div>
       <div className="form-buttons">
-        <button onClick={toggleForm}>
+        <button onClick={toggleForm} type="button">
           Cancel <FcCancel />
         </button>
         <button className="add-card-button">
-          Add Card <MdOutlineAddCard />{" "}
+          Add Card <MdOutlineAddCard />
         </button>
       </div>
     </form>
